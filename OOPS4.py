f8dd5754193e34a2bb1b88dc0d5fe0471933cdf0
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-02-29 21:01:38 +0530

    Update OOPS4.py
    Program using instance method

--- OOPS4.py
+++ OOPS4.py
@@ -1,4 +1,16 @@
 class student:
 
     def __init__(self,m1,m2,m3):
-        self.
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+    
+    def avg(self):
+        return (self.m1 + self.m2 + self.m3)/3
+    
+    
+s1 = student()
+s2 = student()
+
+s1.avg(10,20,30)
+s2.avg(20,30,40)
